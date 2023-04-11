0408981ae413711fedc853f47b1a981a6f0c4d0d
shivani0029 <130443547+shivani0029@users.noreply.github.com>
2023-04-11 14:02:25 +0530

    Create 5.double value from the user.java

---
+++ Week1 Day2: Writing your First Java Program and Primitive Data Types/5.double value from the user.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){    	     
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a double value");
+        double n=sc.nextDouble();
+        
+        System.out.println("Double value ="+n);
+        System.out.println("Typecasted to float: Value = "+(float)n);
+        System.out.println("Typecasted to long: Value = "+(long)n);
+        System.out.println("Typecasted to int: Value = "+(int)n);
+        System.out.println("Typecasted to short: Value = "+(short)n);
+        System.out.println("Typecasted to byte: Value = "+(byte)n);
+
+
+
+           //Fill your code
+    }
+}
