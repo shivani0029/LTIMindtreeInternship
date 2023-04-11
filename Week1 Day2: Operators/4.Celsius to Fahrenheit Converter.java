a0f2c70aa71fc5643715bbe90482faf8b9bd6cab
shivani0029 <130443547+shivani0029@users.noreply.github.com>
2023-04-11 14:17:17 +0530

    Create 4.Celsius to Fahrenheit Converter.java

---
+++ Week1 Day2: Operators/4.Celsius to Fahrenheit Converter.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) {
+        double c;
+        Scanner sc=new Scanner(System.in);
+        double temp=sc.nextDouble();
+        c=(temp*9/5)+32;
+        System.out.println("Temperature in Celsius:");
+        System.out.println("Temperature in Fahrenheit is "+c+"F");
+
+
+        
+            // Fill your code here
+        
+     }
+}
