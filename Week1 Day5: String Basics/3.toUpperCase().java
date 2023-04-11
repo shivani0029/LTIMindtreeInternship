a351c1c0f6471c9d9f6575b8783f495ef8a82733
shivani0029 <130443547+shivani0029@users.noreply.github.com>
2023-04-11 14:49:51 +0530

    Create 3.toUpperCase().java

---
+++ Week1 Day5: String Basics/3.toUpperCase().java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main {
+	public static void main(String []args){
+		System.out.println("Enter the name:");
+		Scanner sc=new Scanner(System.in);
+		String s=sc.nextLine();
+		System.out.println("Name:"+s.toUpperCase());
+		//fill your code here
+	}
+}
