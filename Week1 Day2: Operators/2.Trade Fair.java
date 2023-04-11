c78bf6eda2b1b5e2ad4eec0bd794d9c7b60fce39
shivani0029 <130443547+shivani0029@users.noreply.github.com>
2023-04-11 14:14:49 +0530

    Create 2.Trade Fair.java

---
+++ Week1 Day2: Operators/2.Trade Fair.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){    	
+        int x;
+        Scanner sc=new Scanner(System.in);
+        int tot=sc.nextInt();
+
+        System.out.println("Enter the total number of people");
+        
+        x=(2*tot)/7;
+        int y=2*x;
+        int z=x/2;
+        System.out.println("Number of attendees on day 1 : "+x);
+        System.out.println("Number of attendees on day 2 : "+y);
+        System.out.println("Number of attendees on day 3 : "+z);
+
+
+
+           //Fill your code
+           }
+           }
