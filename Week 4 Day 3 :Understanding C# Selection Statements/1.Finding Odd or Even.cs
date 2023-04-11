82e17d8476f29c80bc2e2838e17f71deef0db1aa
shivani0029 <130443547+shivani0029@users.noreply.github.com>
2023-04-11 21:02:56 +0530

    Create 1.Finding Odd or Even.cs

---
+++ Week 4 Day 3 :Understanding C# Selection Statements/1.Finding Odd or Even.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Program {
+    public static void Main(String[] args) {
+        int n=Convert.ToInt32(Console.ReadLine());
+        if(n%2==0)
+        {
+            Console.WriteLine(n+" is an even number");
+        }
+        else
+        {
+        Console.WriteLine(n+" is an  odd number");
+        }
+
+        //Fill your code here
+    }
+}
