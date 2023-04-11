8110866b5176a1122ee0d3875db63a0aae6a69d9
shivani0029 <130443547+shivani0029@users.noreply.github.com>
2023-04-11 14:05:31 +0530

    Create 1.Help the Mess Owner.java

---
+++ Week1 Day 2:Week1 Day2: Operators/1.Help the Mess Owner.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){    	     
+        int  totcost;
+        Scanner sc=new Scanner(System.in);
+        int cost=sc.nextInt();
+        int  num=sc.nextInt();
+        totcost=cost*num;
+        System.out.println("Cost of item:");
+        
+         System.out.println("Number of items:");
+          
+          System.out.println("Total cost is Rs."+totcost);
+
+
+
+           //Fill your code
+    }
+}
