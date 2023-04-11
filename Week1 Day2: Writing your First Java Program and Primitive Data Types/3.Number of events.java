5c91dbb9fc3a993f355dfbb9f8d5ad4dea1a3c19
shivani0029 <130443547+shivani0029@users.noreply.github.com>
2023-04-11 14:01:29 +0530

    Create 3.Number of events.java

---
+++ Week1 Day2: Writing your First Java Program and Primitive Data Types/3.Number of events.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number of events hosted in Codissia");
+		int n=sc.nextInt();
+		System.out.println("Number of events hosted in Codissia is"+" "+n);
+		// your code here
+
+	}
+
+}
