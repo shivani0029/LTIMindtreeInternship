a11a62f6fc3e3933afa881b3e3a3c043188f68b3
shivani0029 <130443547+shivani0029@users.noreply.github.com>
2023-04-11 20:56:51 +0530

    Create 2.Customized Welcome Message.cs

---
+++ Week 4 Day 3 : Understanding C# Variables/2.Customized Welcome Message.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program {
+    public static void Main(String[] args) {
+        Console.WriteLine("Enter your name");
+        String n=Console.ReadLine();
+        
+        Console.WriteLine("Hello "+n+"!"+" Welcome to Event Management System.");
+        //Fill your code here
+    }
+}
