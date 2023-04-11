36b3368caca30833d81752ed9c9ffb0c667921ad
shivani0029 <130443547+shivani0029@users.noreply.github.com>
2023-04-11 14:21:20 +0530

    Create 2.DIVISIBLE BY 2 OR 3.java

---
+++ Week1 Day2: Conditional Statements/2.DIVISIBLE BY 2 OR 3.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new  Scanner(System.in);
+        int n=sc.nextInt();
+        if(n%2==0||n%3==0)
+        {
+            System.out.println("yes");
+        
+        }
+        else
+        {
+            System.out.println("no");
+        }
+        
+            // Fill your code here
+        
+     }
+}
