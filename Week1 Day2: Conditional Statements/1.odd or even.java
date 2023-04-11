9c065fe85e67016e784ae6ed588636ad0f0ac38b
shivani0029 <130443547+shivani0029@users.noreply.github.com>
2023-04-11 14:20:35 +0530

    Create 1.odd or even.java

---
+++ Week1 Day2: Conditional Statements/1.odd or even.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);;
+        int n=sc.nextInt();
+        if(n%2==0)
+        {
+            System.out.println(n+" is an even number");
+        }
+        else
+        {
+            System.out.println(n+" is an odd number ");
+        }
+        
+            // Fill your code here
+        
+     }
+}
