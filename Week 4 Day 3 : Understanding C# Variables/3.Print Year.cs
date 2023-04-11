910684e202be3a77b253041b6856ba731019fc40
shivani0029 <130443547+shivani0029@users.noreply.github.com>
2023-04-11 20:57:23 +0530

    Create 3.Print Year.cs

---
+++ Week 4 Day 3 : Understanding C# Variables/3.Print Year.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+     static void Main(string[] args)
+    {
+        Console.WriteLine("Enter current year :");
+        int n=Convert.ToInt32(Console.ReadLine());
+        int y=2006;
+        Console.WriteLine("C# was created in the year "+y+". I am reading C# in the year "+n);
+    }
+}
