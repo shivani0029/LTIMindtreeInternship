0648793aeecfb190b358879c4aa0d185a09e5bcf
shivani0029 <130443547+shivani0029@users.noreply.github.com>
2023-04-11 14:22:10 +0530

    Create 3.Checking Divisibility By 7 and 3.java

---
+++ Week1 Day2: Conditional Statements/3.Checking Divisibility By 7 and 3.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if(n%7==0&&n%3==0)
+        {
+            System.out.println(n+" is divisible by both 7 and 3");
+        }
+        else
+        {
+        System.out.println(n+" is not divisible by both 7 and 3");
+        
+            // Fill your code here
+            }
+        
+     }
+}
