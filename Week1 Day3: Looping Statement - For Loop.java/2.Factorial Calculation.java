663bacd030728b08551042baf0b960f3cdf80e31
shivani0029 <130443547+shivani0029@users.noreply.github.com>
2023-04-11 14:26:08 +0530

    Create 2.Factorial Calculation.java

---
+++ Week1 Day3: Looping Statement - For Loop.java/2.Factorial Calculation.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        int fact=1;
+        Scanner sc=new Scanner (System.in);
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++)
+        {
+            fact=fact*i;
+        }
+        System.out.println(fact);
+
+        
+            // Fill your code here
+        
+     }
+}
